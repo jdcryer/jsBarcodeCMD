4ab4acab581e7bd2bb858310d5dc4dbd1f3eae9d
Joseph Cryer <jcryer@Josephs-MacBook-Pro.local>
2022-07-19 11:36:23 +0100

    Added png creation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var JsBarcode = require("jsbarcode");
+const fs = require("fs");
+
 const { DOMImplementation, XMLSerializer } = require("xmldom");
 var argv = require("minimist")(process.argv.slice(2));
 
@@ -34,7 +36,19 @@ try {
 
 try {
   const svgText = xmlSerializer.serializeToString(svgNode);
-  console.log(svgText);
+  //fs.writeFileSync("out.svg", svgText);
+  if (argv[`codec`] === "svg") {
+    console.log(svgText);
+  } else if (argv[`codec`] === "png") {
+    const sharp = require("sharp");
+    sharp(Buffer.from(svgText))
+      .png()
+      .toBuffer()
+      .then((data) => console.log(data.toString("base64")))
+      .catch((err) => {
+        throw new Error(`Failed to convert barcode SVG to PNG: \n\n${err}`);
+      });
+  }
 } catch (err) {
   throw new Error(`Failed to turn XML to string: \n\n${err}`);
 }
